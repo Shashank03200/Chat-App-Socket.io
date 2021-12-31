3465c25dfd4c8c7b60f7c63a0c0f83e02ca13b94
Shashank Mishra <shashank032000@gmail.com>
2021-12-31 20:09:22 +0530

    Version 1.0

--- server.js
+++ server.js
@@ -1,44 +1,38 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-const server = app.listen(3000, ()=>{
-    console.log("Server running on port 3000");
-})
+const server = app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
 
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
 
-app.get("/", (req, res)=>{
-    res.sendFile(__dirname+"/index.html");
-})
+let users = {};
 
-
-let users  = {};
-
-var socket = require('socket.io');
+var socket = require("socket.io");
 
 // Global socket server object;
 var io = socket(server);
 
-io.sockets.on('connection', (socket)=>{
+io.sockets.on("connection", (socket) => {
+  console.log("New device connected", socket.id);
 
-    console.log('New device connected', socket.id);
+  socket.on("new-user-joined", (name) => {
+    users[socket.id] = name;
+    console.log(users);
+    socket.emit("you-joined", "You have joined the chat");
+    socket.broadcast.emit("user-joined", `${name} joined the chat`);
+  });
 
-    socket.on('new-user-joined',(name)=>{
-        
-        users[socket.id] = name;
-        console.log(users);
-        socket.emit('you-joined','You have joined the chat');
-        socket.broadcast.emit('user-joined',`${name} joined the chat`);
-        
-        
-    })
-
-    socket.on('message-sent', (message)=> {
-        socket.broadcast.emit('message-received',`${users[socket.id]}: ${message}`);
-    })
-
-})
-
-
+  socket.on("message-sent", (message) => {
+    socket.broadcast.emit(
+      "message-received",
+      `${users[socket.id]}: ${message}`
+    );
+  });
+});
